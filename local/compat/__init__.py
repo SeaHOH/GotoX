a26859430acea95a15f5325aa786fb4f89882a08
SeaHOH <Seahoh@Gmail.com>
2016-10-28 04:55:40 +0800

    文件调整

---
+++ local/compat/__init__.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+
+import sys
+import os
+import socket
+import ssl
+_ver = sys.version_info
+PY3 = _ver[0] == 3
+PY35 = PY3 and _ver[2] == 5
+if PY3:
+    import queue as Queue
+    import _thread as thread
+    import http.server as BaseHTTPServer
+    import http.client as httplib
+    import urllib.request as urllib2
+    import urllib.parse as urlparse
+    import socketserver as SocketServer
+    from configparser import ConfigParser
+    xrange = range
+    exc_clear = lambda: None
+    #可添加属性
+    class socketMod(socket.socket): pass
+    socket.socket = socketMod
+    #默认编码
+    _read = ConfigParser.read
+    ConfigParser.read = lambda *args, encoding='utf8': _read(*args, encoding=encoding)
+else:
+    import Queue
+    import thread
+    import BaseHTTPServer
+    import httplib
+    import urllib2
+    import urlparse
+    import SocketServer
+    from ConfigParser import ConfigParser
+    xrange = xrange
+    exc_clear = sys.exc_clear
---
+++ local/compat/openssl.py
@@ -0,0 +1,138 @@
+# coding:utf-8
+'''OpenSSL Connection Wrapper'''
+
+import socket
+import OpenSSL
+from select import select
+from compat import PY3, exc_clear
+
+class SSLConnection(object):
+    '''API-compatibility wrapper for Python OpenSSL's Connection-class.'''
+
+    def __init__(self, context, sock):
+        self._context = context
+        self._sock = sock
+        self._connection = OpenSSL.SSL.Connection(context, sock)
+        self._io_refs = 0
+
+    def __del__(self):
+        if self._sock:
+            self._sock.close()
+            self._sock = None
+
+    def __getattr__(self, attr):
+        if attr not in ('_context', '_sock', '_connection', '_io_refs'):
+            return getattr(self._connection, attr)
+
+    def __iowait(self, io_func, *args, **kwargs):
+        timeout = self._sock.gettimeout()
+        fd = self._sock
+        while self._connection:
+            try:
+                return io_func(*args, **kwargs)
+            except (OpenSSL.SSL.WantReadError, OpenSSL.SSL.WantX509LookupError):
+                exc_clear()
+                rd, _, ed = select([fd], [], [fd], timeout)
+                if ed:
+                    raise socket.error(ed)
+                if not rd:
+                    raise socket.timeout('The read operation timed out')
+            except OpenSSL.SSL.WantWriteError:
+                exc_clear()
+                _, wd, ed = select([], [fd], [fd], timeout)
+                if ed:
+                    raise socket.error(ed)
+                if not wd:
+                    raise socket.timeout('The write operation timed out')
+            except OpenSSL.SSL.SysCallError as e:
+                if e.args[0] == 10035 and 'WSAEWOULDBLOCK' in e.args[1]:
+                    exc_clear()
+                    rd, wd, ed = select([fd], [fd], [fd], timeout)
+                    if ed:
+                        raise socket.error(ed)
+                    if not rd and not wd:
+                        raise socket.timeout('The socket operation timed out')
+                else:
+                    raise e
+            except Exception as e:
+                raise e
+
+    def accept(self):
+        sock, addr = self._sock.accept()
+        client = SSLConnection(self._context, sock)
+        client.set_accept_state()
+        return client, addr
+
+    def do_handshake(self):
+        self.__iowait(self._connection.do_handshake)
+
+    def connect(self, addr):
+        self.__iowait(self._connection.connect, addr)
+
+    def send(self, data, flags=0):
+        try:
+            return self.__iowait(self._connection.send, data)
+        except OpenSSL.SSL.SysCallError as e:
+            if e.args[0] == -1 and not data:
+                return 0
+            raise socket.error(str(e))
+        except Exception as e:
+            raise socket.error(str(e))
+
+    def sendall(self, data, flags=0):
+        total_sent = 0
+        total_to_send = len(data)
+        while total_sent < total_to_send:
+            sent = self.send(data[total_sent:total_sent + 16384]) # 16K
+            total_sent += sent
+    write = sendall
+
+    def recv(self, bufsiz, flags=None):
+        pending = self._connection.pending()
+        if pending:
+            return self._connection.recv(min(pending, bufsiz))
+        try:
+            return self.__iowait(self._connection.recv, bufsiz, flags)
+        except OpenSSL.SSL.ZeroReturnError as e:
+            if self._connection.get_shutdown() == OpenSSL.SSL.RECEIVED_SHUTDOWN:
+                return b''
+            raise e
+        except OpenSSL.SSL.SysCallError as e:
+            if e.args[0] == -1 and 'Unexpected EOF' in e.args[1]:
+                return b''
+            elif e.args[0] in (10053, 10054, 10038):
+                return b''
+            raise socket.error(str(e))
+    read = recv
+
+    def recv_into(self, buffer, nbytes=None, flags=None):
+        try:
+            return self.__iowait(self._connection.recv_into, buffer, nbytes, flags)
+        except OpenSSL.SSL.ZeroReturnError as e:
+            if self._connection.get_shutdown() == OpenSSL.SSL.RECEIVED_SHUTDOWN:
+                return 0
+            raise e
+        except OpenSSL.SSL.SysCallError as e:
+            if e.args[0] == -1 and 'Unexpected EOF' in e.args[1]:
+                return 0
+            elif e.args[0] in (10053, 10054, 10038):
+                return 0
+            raise socket.error(str(e))
+
+    def close(self):
+        if self._io_refs < 1:
+            self._connection = None
+            if self._sock:
+                self._sock.close()
+                self._sock = None
+        else:
+            self._io_refs -= 1
+
+    if PY3:
+        def makefile(self, *args, **kwargs):
+            return socket.socket.makefile(self, *args, **kwargs)
+    else:
+        def makefile(self, mode='r', bufsize=-1):
+            self._io_refs += 1
+            return socket._fileobject(self, mode, bufsize, close=True)
+            
