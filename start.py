d85d9204c158a756ad57b948799c150caa00b4d8
SeaHOH <Seahoh@Gmail.com>
2016-10-29 07:42:21 +0800

    Add files via upload

--- start.py
+++ start.py
@@ -3,6 +3,11 @@
 
 import os
 import sys
+
+__file__ = os.path.abspath(__file__)
+if os.path.islink(__file__):
+    __file__ = getattr(os, 'readlink', lambda x: x)(__file__)
+
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from local import proxy
 proxy.main()
