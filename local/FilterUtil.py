470a6967a27ecb7aa81711943c4cf4bb9296362f
SeaHOH <Seahoh@Gmail.com>
2016-11-19 20:31:01 +0800

    修复重定向匹配 bug，改进数据结构

--- local/FilterUtil.py
+++ local/FilterUtil.py
@@ -1,13 +1,34 @@
 # coding:utf-8
 
 from time import time
-from collections import OrderedDict
-from .common import LRUCache
+from functools import partial
+from . import clogging as logging
+from .common import config_dir, LRUCache
+from .compat import urlparse
 from .GlobalConfig import GC
-from .FilterConfig import FAKECERT, numToAct, numToSSLAct, ACTION_FILTERS
+from .FilterConfig import (
+    FAKECERT,
+    numToAct,
+    numToSSLAct,
+    ACTION_FILTERS
+    )
 
 filters_cache = LRUCache(64)
 ssl_filters_cache = LRUCache(32)
+
+def get_redirect(target, url):
+    '''Get the redirect target'''
+    rule = target[0]
+    if isinstance(rule, partial):
+        url = rule(url, 1)
+    elif isinstance(rule, tuple):
+        url = url.replace(*rule)
+    elif isinstance(target, str) and target.find('://') < 9:
+        return target
+    else:
+        logging.error(u'%r 匹配重定向规则 %r，解析错误，请检查你的配置文件："%s/ActionFilter.ini"', url, target, config_dir)
+        return
+    return urlparse.unquote(url) if target[1] else url
 
 def match_host_filter(filter, host):
     if isinstance(filter, str):
@@ -27,50 +48,61 @@ def match_path_filter(filter, path):
         return filter in path
     return filter(path)
 
-def get_action(scheme, host, path):
+REDIRECTS = ('do_REDIRECT', 'do_IREDIRECT')
+TEMPGAE = 'do_GAE', None
+#默认规则
+filter_DEF = '', '', numToAct[GC.FILTER_ACTION], None
+ssl_filter_DEF = numToSSLAct[GC.FILTER_SSLACTION], None
+
+def get_action(scheme, host, path, url):
     schemes = ('', scheme)
     key = scheme + host
-    if key in filters_cache:
+    filters = filters_cache.get(key)
+    if filters:
         #以缓存规则进行匹配
-        filters = filters_cache[key]
-        for filter, rule in filters.items():
-            if rule[1] in schemes and match_path_filter(filter, path):
+        for schemefilter, pathfilter, action, target in filters:
+            if schemefilter in schemes and match_path_filter(pathfilter, path):
+                #计算重定向网址
+                if action in REDIRECTS:
+                    durl = get_redirect(target, url)
+                    if durl and durl != url:
+                        return action, durl
+                    else:
+                        continue
                 #是否临时规则
-                target = rule[0][1]
                 if isinstance(target, float):
                     # 15 分钟后恢复默认规则
                     if time() - target > 900:
-                        filters[filter] = (numToAct[GC.FILTER_ACTION], None), ''
+                        filters[-1] = filter_DEF
                     #自动多线程不持续使用临时 GAE 规则，仍尝试默认设置
                     #不包含元组元素（媒体文件）
                     elif not any(x in path for x in GC.AUTORANGE_ENDSWITH):
-                        return 'do_GAE', ''
-                return rule[0]
+                        return TEMPGAE
+                return action, target
     else:
         filter = None
+        #建立缓存条目
+        filters_cache[key] = []
         for filters in ACTION_FILTERS:
             if filters.action == FAKECERT:
                 continue
             for schemefilter, hostfilter, pathfilter, target in filters:
                 if schemefilter in schemes and match_host_filter(hostfilter, host):
-                    #建立主机条目
-                    if key not in filters_cache:
-                        filters_cache[key] = OrderedDict()
+                    action = numToAct[filters.action]
                     #填充规则到缓存
-                    if pathfilter not in filters_cache.cache[key]:
-                        filters_cache.cache[key][pathfilter] = (numToAct[filters.action], target), schemefilter
+                    filters_cache.cache[key].append((schemefilter, pathfilter, action, target))
                     #匹配第一个，后面忽略
                     if not filter and match_path_filter(pathfilter, path):
-                        filter = numToAct[filters.action], target
-        if filter:
-            return filter
-    #构建默认规则
-    filter = (numToAct[GC.FILTER_ACTION], None), ''
-    if key not in filters_cache:
-        #只有一条规则使用普通字典
-        filters_cache[key] = {}
-    filters_cache[key][''] = filter
-    return filter[0]
+                        #计算重定向网址
+                        if action in REDIRECTS:
+                            durl = get_redirect(target, url)
+                            if durl and durl != url:
+                                filter = action, durl
+                        else:
+                            filter = action, target
+        #添加默认规则
+        filters_cache.cache[key].append(filter_DEF)
+        return filter or (filter_DEF[2], filter_DEF[3])
 
 def get_ssl_action(ssl, host):
     schemes = ('', 'https' if ssl else 'http')
@@ -84,6 +116,6 @@ def get_ssl_action(ssl, host):
                     ssl_filters_cache[host] = filter = numToSSLAct[filters.action], target
                     #匹配第一个，后面忽略
                     return filter
-        #构建默认规则
-        ssl_filters_cache[host] = filter = numToSSLAct[GC.FILTER_SSLACTION], None
-        return filter
+        #添加默认规则
+        ssl_filters_cache[host] = ssl_filter_DEF
+        return ssl_filter_DEF
