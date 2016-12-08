db5cde7f607656a7e3d9423fc9b01a7296ec6b32
SeaHOH <Seahoh@Gmail.com>
2016-12-08 22:15:34 +0800

    运行中可以修改代理规则，修复从临时规则恢复到默认规则时的错误

--- local/FilterConfig.py
+++ local/FilterConfig.py
@@ -2,8 +2,10 @@
 
 import os
 import re
+import threading
 from functools import partial
-from .compat import ConfigParser
+from time import sleep
+from .compat import thread, ConfigParser
 from .common import config_dir, isipv4, isipv6
 from .GlobalConfig import GC
 
@@ -61,64 +63,85 @@ elif GC.LINK_PROFILE == 'ipv6':
 
 class classlist(list): pass
 
-ACTION_FILTERS = classlist()
-ACTION_FILTERS.reset = False
-CONFIG = ConfigParser()
-CONFIG._optcre = re.compile(r'(?P<option>[^\s]+)(?P<vi>\s+=)?\s*(?P<value>.*)')
-ACTION_FILTERS.CONFIG_FILENAME = os.path.join(config_dir, 'ActionFilter.ini')
-CONFIG.read(ACTION_FILTERS.CONFIG_FILENAME)
+class actionfilterlist(list):
 
-sections = CONFIG.sections()
-sections.sort()
-for s in sections:
-    try:
-        order, action = isfiltername(s).group('order', 'action')
-    except:
-        continue
-    action = action.upper()
-    if action not in actToNum:
-        continue
-    #order = int(order)
-    filters = classlist()
-    filters.action = actToNum[action]
-    #print('[%s]' % s, filters.action)
-    for k, v in CONFIG.items(s):
-        scheme = ''
-        if k.find('://', 0, 9) > 0 :
-            scheme, _, k = k.partition('://')
-        if  '/' in  k:
-            host, _, path = k.partition('/')
-        else:
-            host, path = k, ''
-        if host.find('@') == 0:
-            host = re.compile(host[1:]).search
-        if path.find('@') == 0:
-            path = re.compile(path[1:]).search
-        if v and filters.action in (2, 3):
-            if '|' in v:
-                v = pickip(' '+v.lower()) or ''
-            elif ipnotuse(v):
-                v = ''
-        elif filters.action in (7, 8) and '>>' in v:
-            patterns, _, replaces = v.partition('>>')
-            patterns = patterns.rstrip(' \t')
-            replaces = replaces.lstrip(' \t')
-            if patterns[0] == '@':
-                patterns = patterns[1:].lstrip(' \t')
-                if replaces[0] == '@':
-                    replaces = replaces[1:].lstrip(' \t')
-                    v = partial(re.compile(patterns).sub, replaces), True
+    CONFIG_FILENAME = os.path.join(config_dir, 'ActionFilter.ini')
+
+    def __init__(self):
+        list.__init__(self)
+        self.readconfig()
+        self.FILE_MTIME = os.path.getmtime(self.CONFIG_FILENAME)
+        self.RESET = False
+        thread.start_new_thread(self.check_modify, ())
+
+    def readconfig(self):
+        CONFIG = ConfigParser()
+        CONFIG._optcre = re.compile(r'(?P<option>[^\s]+)(?P<vi>\s+=)?\s*(?P<value>.*)')
+        CONFIG.read(self.CONFIG_FILENAME)
+
+        sections = CONFIG.sections()
+        sections.sort()
+        self.clear()
+        for s in sections:
+            try:
+                order, action = isfiltername(s).group('order', 'action')
+            except:
+                continue
+            action = action.upper()
+            if action not in actToNum:
+                continue
+            #order = int(order)
+            filters = classlist()
+            filters.action = actToNum[action]
+            #print('[%s]' % s, filters.action)
+            for k, v in CONFIG.items(s):
+                scheme = ''
+                if k.find('://', 0, 9) > 0 :
+                    scheme, _, k = k.partition('://')
+                if  '/' in  k:
+                    host, _, path = k.partition('/')
                 else:
-                    v = partial(re.compile(patterns).sub, replaces), False
-            else:
-                if replaces[0] == '@':
-                    replaces = replaces[1:].lstrip(' \t')
-                    v = (patterns, replaces, 1), True
-                else:
-                    v = (patterns, replaces, 1), False
-        #print(host, path, v)
-        #print('@'+host.__self__.pattern if isinstance(host, dir.__class__) else host,
-        #      '@'+url.__self__.pattern if isinstance(url, dir.__class__) else url,
-        #      v)
-        filters.append((scheme.lower(), host, path, v))
-    ACTION_FILTERS.append(filters)
+                    host, path = k, ''
+                if host.find('@') == 0:
+                    host = re.compile(host[1:]).search
+                if path.find('@') == 0:
+                    path = re.compile(path[1:]).search
+                if v and filters.action in (2, 3):
+                    if '|' in v:
+                        v = pickip(' '+v.lower()) or ''
+                    elif ipnotuse(v):
+                        v = ''
+                elif filters.action in (7, 8) and '>>' in v:
+                    patterns, _, replaces = v.partition('>>')
+                    patterns = patterns.rstrip(' \t')
+                    replaces = replaces.lstrip(' \t')
+                    if patterns[0] == '@':
+                        patterns = patterns[1:].lstrip(' \t')
+                        if replaces[0] == '@':
+                            replaces = replaces[1:].lstrip(' \t')
+                            v = partial(re.compile(patterns).sub, replaces), True
+                        else:
+                            v = partial(re.compile(patterns).sub, replaces), False
+                    else:
+                        if replaces[0] == '@':
+                            replaces = replaces[1:].lstrip(' \t')
+                            v = (patterns, replaces, 1), True
+                        else:
+                            v = (patterns, replaces, 1), False
+                #print(host, path, v)
+                #print('@'+host.__self__.pattern if isinstance(host, dir.__class__) else host,
+                #      '@'+url.__self__.pattern if isinstance(url, dir.__class__) else url,
+                #      v)
+                filters.append((scheme.lower(), host, path, v))
+            self.append(filters)
+
+    def check_modify(self):
+        while True:
+            filemtime = os.path.getmtime(self.CONFIG_FILENAME)
+            if filemtime > self.FILE_MTIME and not self.RESET:
+                self.FILE_MTIME = filemtime
+                self.readconfig()
+                self.RESET = True
+            sleep(10)
+
+ACTION_FILTERS = actionfilterlist()
--- local/FilterUtil.py
+++ local/FilterUtil.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 
+import threading
 from time import time
 from functools import partial
 from . import clogging as logging
@@ -10,11 +11,24 @@ from .FilterConfig import (
     FAKECERT,
     numToAct,
     numToSSLAct,
-    ACTION_FILTERS
+    ACTION_FILTERS as _ACTION_FILTERS
     )
 
+gLock = threading.Lock()
+gn = 0
+ACTION_FILTERS = _ACTION_FILTERS.copy()
 filters_cache = LRUCache(64)
 ssl_filters_cache = LRUCache(32)
+
+def check_reset():
+    with gLock:
+        if gn == 0 and _ACTION_FILTERS.RESET:
+            global ACTION_FILTERS
+            _ACTION_FILTERS.RESET = False
+            ACTION_FILTERS = _ACTION_FILTERS.copy()
+            filters_cache.clear()
+            ssl_filters_cache.clear()
+            logging.info('%r 内容被修改，已重新加载配置。', _ACTION_FILTERS.CONFIG_FILENAME)
 
 def get_redirect(target, url):
     '''Get the redirect target'''
@@ -55,6 +69,7 @@ filter_DEF = '', '', numToAct[GC.FILTER_ACTION], None
 ssl_filter_DEF = numToSSLAct[GC.FILTER_SSLACTION], None
 
 def get_action(scheme, host, path, url):
+    check_reset()
     schemes = ('', scheme)
     key = scheme + host
     filters = filters_cache.get(key)
@@ -74,6 +89,7 @@ def get_action(scheme, host, path, url):
                     # 15 分钟后恢复默认规则
                     if time() - target > 900:
                         filters[-1] = filter_DEF
+                        return filter_DEF[2:]
                     #符合自动多线程时不使用临时 GAE 规则，仍尝试默认规则
                     #是否包含元组元素（媒体文件）
                     elif any(path.endswith(x) for x in GC.AUTORANGE_ENDSWITH):
@@ -82,42 +98,57 @@ def get_action(scheme, host, path, url):
                         return TEMPGAE
                 return action, target
     else:
-        filter = None
-        #建立缓存条目
-        filters_cache[key] = []
-        for filters in ACTION_FILTERS:
-            if filters.action == FAKECERT:
-                continue
-            for schemefilter, hostfilter, pathfilter, target in filters:
-                if schemefilter in schemes and match_host_filter(hostfilter, host):
-                    action = numToAct[filters.action]
-                    #填充规则到缓存
-                    filters_cache.cache[key].append((schemefilter, pathfilter, action, target))
-                    #匹配第一个，后面忽略
-                    if not filter and match_path_filter(pathfilter, path):
-                        #计算重定向网址
-                        if action in REDIRECTS:
-                            durl = get_redirect(target, url)
-                            if durl and durl != url:
-                                filter = action, durl
-                        else:
-                            filter = action, target
-        #添加默认规则
-        filters_cache.cache[key].append(filter_DEF)
-        return filter or filter_DEF[2:]
+        global gn
+        try:
+            with gLock:
+                gn += 1
+            filter = None
+            #建立缓存条目
+            filters_cache[key] = []
+            for filters in ACTION_FILTERS:
+                if filters.action == FAKECERT:
+                    continue
+                for schemefilter, hostfilter, pathfilter, target in filters:
+                    if schemefilter in schemes and match_host_filter(hostfilter, host):
+                        action = numToAct[filters.action]
+                        #填充规则到缓存
+                        filters_cache.cache[key].append((schemefilter, pathfilter, action, target))
+                        #匹配第一个，后面忽略
+                        if not filter and match_path_filter(pathfilter, path):
+                            #计算重定向网址
+                            if action in REDIRECTS:
+                                durl = get_redirect(target, url)
+                                if durl and durl != url:
+                                    filter = action, durl
+                            else:
+                                filter = action, target
+            #添加默认规则
+            filters_cache.cache[key].append(filter_DEF)
+            return filter or filter_DEF[2:]
+        finally:
+            with gLock:
+                gn -= 1
 
 def get_connect_action(ssl, host):
+    check_reset()
     schemes = ('', 'https' if ssl else 'http')
     if host in ssl_filters_cache:
         return ssl_filters_cache[host]
     else:
-        for filters in ACTION_FILTERS:
-            for schemefilter, hostfilter, _, target in filters:
-                if schemefilter in schemes and match_host_filter(hostfilter, host):
-                    #填充结果到缓存
-                    ssl_filters_cache[host] = filter = numToSSLAct[filters.action], target
-                    #匹配第一个，后面忽略
-                    return filter
-        #添加默认规则
-        ssl_filters_cache[host] = ssl_filter_DEF
-        return ssl_filter_DEF
+        global gn
+        try:
+            with gLock:
+                gn += 1
+            for filters in ACTION_FILTERS:
+                for schemefilter, hostfilter, _, target in filters:
+                    if schemefilter in schemes and match_host_filter(hostfilter, host):
+                        #填充结果到缓存
+                        ssl_filters_cache[host] = filter = numToSSLAct[filters.action], target
+                        #匹配第一个，后面忽略
+                        return filter
+            #添加默认规则
+            ssl_filters_cache[host] = ssl_filter_DEF
+            return ssl_filter_DEF
+        finally:
+            with gLock:
+                gn -= 1
