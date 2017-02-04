941cc2c1b42540cf8c16d3795b616e106908307a
SeaHOH <Seahoh@Gmail.com>
2017-02-04 22:41:50 +0800

    Revert "修复临时 GAE 规则未作用于 do_CONNECT 方法"
    This reverts commit c8021c70bde69b92b26b5b067646b14eae1758fa.

--- local/ProxyHandler.py
+++ local/ProxyHandler.py
@@ -36,7 +36,6 @@ from .RangeFetch import RangeFetch
 from .GAEFetch import qGAE, gae_urlfetch
 from .FilterUtil import (
     filters_cache,
-    ssl_filters_cache,
     get_action,
     get_connect_action
     )
@@ -767,11 +766,9 @@ class AutoProxyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         #最近是否失败（缓存设置超时两分钟）
         if host in self.badhost:
             if self.badhost[host]:
-                #记录临时规则的过期时间
+                #记录临时规则加入时间
                 key = self.url_parts.scheme + host
-                now = time() + 900
-                filters_cache[key][-1] = '', '', 'TEMPGAE', now
-                ssl_filters_cache[host] = 'TEMPGAE', now
+                filters_cache[key][-1] = '', '', 'TEMPGAE', time()
                 logging.warning('将 %r 加入 "GAE" 规则 15 分钟。', host)
                 self.badhost[host] = False
         else:
