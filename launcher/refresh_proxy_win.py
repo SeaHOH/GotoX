7cb3f14e3ce7c2d79dd6af1fab0905213dbf36a3
SeaHOH <Seahoh@Gmail.com>
2017-03-31 03:11:35 +0800

    使用更灵活的代理设置方式

---
+++ launcher/refresh_proxy_win.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+# 改变注册表设置后
+# HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings
+# 调用此脚本刷新代理状态
+# 同一进程多次刷新可能会失败，保险起见使用进程方式调用
+
+from ctypes import windll
+
+# INTERNET_OPTION_SETTINGS_CHANGED = 39
+# INTERNET_OPTION_PROXY_SETTINGS_CHANGED = 95
+windll.wininet.InternetSetOptionW(0, 95, 0, 0)
--- launcher/win32.py
+++ launcher/win32.py
@@ -16,6 +16,7 @@ app_start = os.path.join(app_root, 'start.py')
 icon_gotox = os.path.join(app_root, 'gotox.ico')
 config_dir = os.path.join(app_root, 'config')
 ipdb_direct = os.path.join(app_root, 'data', 'directip.db')
+refresh_proxy = os.path.join(app_root, 'launcher', 'refresh_proxy_win.py')
 
 #使用安装版 Python
 if os.path.dirname(py_exe) != py_path:
@@ -24,19 +25,15 @@ if os.path.dirname(py_exe) != py_path:
     sys.path.insert(0, helpers)
 sys.path.insert(0, app_root)
 
-from local import __version__ as gotoxver, clogging as logging
-from subprocess import Popen
-import ctypes
-from winsystray import SysTrayIcon, win32_adapter
-import winreg
-from time import sleep
-import win32_proxy_manager as proxy_manager
 import re
-import buildipdb
 from configparser import ConfigParser
 #默认编码
 _read = ConfigParser.read
 ConfigParser.read = lambda s, f, encoding='utf8': _read(s, f, encoding)
+
+CONFIG_FILENAME = os.path.join(config_dir, 'Config.ini')
+CONFIG_USER_FILENAME = re.sub(r'\.ini$', '.user.ini', CONFIG_FILENAME)
+CONFIG_AUTO_FILENAME = os.path.join(config_dir, 'ActionFilter.ini')
 
 def get_listen_addr():
     CONFIG = ConfigParser(inline_comment_prefixes=('#', ';'))
@@ -46,37 +43,127 @@ def get_listen_addr():
     if LISTEN_IP == '0.0.0.0': LISTEN_IP = '127.0.0.1'
     LISTEN_GAE = '%s:%d' % (LISTEN_IP, CONFIG.getint('listen', 'gae_port'))
     LISTEN_AUTO = '%s:%d' % (LISTEN_IP, CONFIG.getint('listen', 'auto_port'))
-    return LISTEN_GAE, LISTEN_AUTO
+    return proxy_server(LISTEN_GAE, True), proxy_server(LISTEN_AUTO, True)
+
+import winreg
+
+SET_PATH = r'Software\Microsoft\Windows\CurrentVersion\Internet Settings'
+SETTINGS = winreg.OpenKey(winreg.HKEY_CURRENT_USER, SET_PATH, 0, winreg.KEY_ALL_ACCESS)
+ProxyOverride = ('localhost;127.*;192.168.*;10.*;172.16.*;172.17.*;172.18.*;'
+                 '172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;'
+                 '172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*')
+
+class proxy_server:
+    __slots__ = 'type', 'pac', 'http', 'https', 'ftp', 'socks'
+
+    def __init__(self, server_str, http_only=None):
+        if not server_str:
+            self.type = 0
+            return
+        if '://' in server_str:
+            self.type = 1
+            self.pac = server_str
+            return
+        elif '=' in server_str:
+            for k, v in (kv.split('=', 1) for kv in server_str.split(';')):
+                self.__setattr__(k, v)
+        else:
+            self.http = server_str
+            self.https = server_str
+            if not http_only:
+                self.ftp = server_str
+                self.socks = server_str
+        self.type = 2
+
+    def __getattr__(self, attr):
+        try:
+            return self.__getattribute__(attr)
+        except:
+            if attr in self.__slots__:
+                return None
+
+    def __contains__(self, obj):
+        if isinstance(obj, str):
+            return obj in self.str
+        if isinstance(obj, self.__class__):
+            return ((not obj.http  or obj.http  == self.http ) and
+                    (not obj.https or obj.https == self.https) and
+                    (not obj.ftp   or obj.ftp   == self.ftp  ) and
+                    (not obj.socks or obj.socks == self.socks))
+
+    def get_server_list(self):
+        server_list = []
+        #按顺序重建
+        for k in ('http', 'https', 'ftp', 'socks'):
+            v = self.__getattr__(k)
+            if v:
+                server_list.append('%s=%s' % (k, v))
+        return server_list
+
+    @property
+    def str(self):
+        #作为设置参数使用
+        if self.type == 0:
+            return ''
+        elif self.type & 1:
+            return self.pac
+        return ';'.join(self.get_server_list())
+
+    def __str__(self):
+        #打印用
+        if self.type == 0:
+            return '无'
+        server_list = self.get_server_list()
+        if self.type & 1:
+            server_list.insert(0, 'pac=' + self.pac)
+        return '\n'.join(server_list)
 
 def get_proxy_state():
+    AutoConfigURL = ProxyServer = None
     try:
         AutoConfigURL, reg_type = winreg.QueryValueEx(SETTINGS, 'AutoConfigURL')
-        return AutoConfigURL
+        AutoConfigURL = proxy_server(AutoConfigURL)
     except:
         pass
     try:
         ProxyEnable, reg_type = winreg.QueryValueEx(SETTINGS, 'ProxyEnable')
         if ProxyEnable:
             ProxyServer, reg_type = winreg.QueryValueEx(SETTINGS, 'ProxyServer')
-            return ProxyServer
+            ProxyServer = proxy_server(ProxyServer)
     except:
         pass
-    return '无'
+    if AutoConfigURL and ProxyServer:
+        ProxyServer.pac = AutoConfigURL.pac
+        ProxyServer.type |= 1
+    elif AutoConfigURL:
+        ProxyServer = AutoConfigURL
+    elif ProxyServer:
+        pass
+    else:
+        ProxyServer = proxy_server(None)
+    return ProxyServer
 
-CONFIG_FILENAME = os.path.join(config_dir, 'Config.ini')
-CONFIG_USER_FILENAME = re.sub(r'\.ini$', '.user.ini', CONFIG_FILENAME)
-CONFIG_AUTO_FILENAME = os.path.join(config_dir, 'ActionFilter.ini')
-SET_PATH = r'Software\Microsoft\Windows\CurrentVersion\Internet Settings'
-SETTINGS = winreg.OpenKey(winreg.HKEY_CURRENT_USER, SET_PATH, 0, winreg.KEY_ALL_ACCESS)
-proxy_state = get_proxy_state()
+def refresh_proxy_state(enable=None):
+    if enable:
+        try:
+            ProxyOverride, reg_type = winreg.QueryValueEx(SETTINGS, 'ProxyOverride')
+        except:
+            ProxyOverride =None
+        #导入默认代理例外地址
+        if not ProxyOverride:
+            winreg.SetValueEx(SETTINGS, 'ProxyOverride', 0,  winreg.REG_SZ, ProxyOverride)
+    Popen((py_exe, refresh_proxy))
+
+from subprocess import Popen
+from local import __version__ as gotoxver, clogging as logging
+
 GotoX_app = None
-hwnd = ctypes.windll.kernel32.GetConsoleWindow()
 
 def start_GotoX():
     global GotoX_app, LISTEN_GAE, LISTEN_AUTO
     LISTEN_GAE, LISTEN_AUTO = get_listen_addr()
     GotoX_app = Popen((py_exe, app_start))
-    os.environ['HTTPS_PROXY'] = os.environ['HTTP_PROXY'] = LISTEN_AUTO
+    os.environ['HTTPS_PROXY'] = os.environ['HTTP_PROXY'] = LISTEN_AUTO.http
 
 def stop_GotoX():
     if GotoX_app is None:
@@ -87,6 +174,9 @@ def stop_GotoX():
             GotoX_app.terminate()
         else:
             logging.warning('GotoX 进程已经结束，code：%s。', retcode)
+
+import ctypes
+hwnd = ctypes.windll.kernel32.GetConsoleWindow()
 
 def on_show(systray):
     ctypes.windll.user32.ShowWindow(hwnd, 1)
@@ -111,13 +201,52 @@ def on_quit(systray):
     sys.exit(0)
 
 def on_disable_proxy(systray):
-    proxy_manager.disable_proxy()
+    proxy_state = proxy_state_menu
+    if proxy_state.type & 1:
+        winreg.DeleteValue(SETTINGS, 'AutoConfigURL')
+    if proxy_state.type & 2:
+        winreg.SetValueEx(SETTINGS, 'ProxyEnable', 0,  winreg.REG_DWORD, 0)
+    if proxy_state.type > 0:
+        refresh_proxy_state()
+
+def disable_x_proxy(type):
+    proxy_state = proxy_state_menu
+    proxy_state.__delattr__(type)
+    #保持代理类型为 Server
+    proxy_state.type = 2
+    if proxy_state.str == '':
+        return on_disable_proxy(None)
+    winreg.SetValueEx(SETTINGS, 'ProxyServer', 0,  winreg.REG_SZ, proxy_state.str)
+    refresh_proxy_state()
+
+def on_disable_http_proxy(systray):
+    disable_x_proxy('http')
+
+def on_disable_https_proxy(systray):
+    disable_x_proxy('https')
+
+def on_disable_ftp_proxy(systray):
+    disable_x_proxy('ftp')
+
+def on_disable_socks_proxy(systray):
+    disable_x_proxy('socks')
+
+def enable_proxy(ProxyServer):
+    proxy_state = proxy_state_menu
+    if proxy_state.pac:
+        winreg.DeleteValue(SETTINGS, 'AutoConfigURL')    
+    if not proxy_state.type & 2:
+        winreg.SetValueEx(SETTINGS, 'ProxyEnable', 0,  winreg.REG_DWORD, 1)
+    proxy_state.http = ProxyServer.http
+    proxy_state.https = ProxyServer.https
+    winreg.SetValueEx(SETTINGS, 'ProxyServer', 0,  winreg.REG_SZ, proxy_state.str)
+    refresh_proxy_state(1)
 
 def on_enable_auto_proxy(systray):
-    proxy_manager.set_proxy(LISTEN_AUTO)
+    enable_proxy(LISTEN_AUTO)
 
 def on_enable_gae_proxy(systray):
-    proxy_manager.set_proxy(LISTEN_GAE)
+    enable_proxy(LISTEN_GAE)
 
 def on_left_click(systray):
     build_menu(systray)
@@ -126,6 +255,9 @@ def on_left_click(systray):
 def on_right_click(systray):
     visible = ctypes.windll.user32.IsWindowVisible(hwnd)
     ctypes.windll.user32.ShowWindow(hwnd, visible^1)
+
+from winsystray import SysTrayIcon, win32_adapter
+import buildipdb
 
 MFS_CHECKED = win32_adapter.MFS_CHECKED
 MFS_DISABLED = win32_adapter.MFS_DISABLED
@@ -144,29 +276,36 @@ sub_menu2 = (('建议更新频率：10～30 天一次', 'pass', MFS_DISABLED),
              ('从以上两者下载后合并', lambda x: buildipdb.download_both_cniplist_as_db(ipdb_direct)))
 
 def build_menu(systray):
-    proxy_state = get_proxy_state()
-    disable_item_state = proxy_state == '无' and fixed_fState or 0
-    auto_item_state = proxy_state == LISTEN_AUTO and fixed_fState or 0
-    gae_item_state = proxy_state == LISTEN_GAE and fixed_fState or 0
-    sub_menu3 = (('禁用代理', on_disable_proxy, disable_item_state, MFT_RADIOCHECK),
-                 ('自动代理', on_enable_auto_proxy, auto_item_state, MFT_RADIOCHECK),
-                 ('GAE 代理', on_enable_gae_proxy, gae_item_state, MFT_RADIOCHECK),
+    global proxy_state_menu, last_main_menu
+    proxy_state_menu = proxy_state = get_proxy_state()
+    disable_state = proxy_state.type == 0 and fixed_fState or 0
+    disable_http_state = disable_state or proxy_state.type & 2 and not proxy_state.http and fixed_fState or 0
+    disable_https_state = disable_state or proxy_state.type & 2 and not proxy_state.https and fixed_fState or 0
+    disable_ftp_state = disable_state or proxy_state.type & 2 and not proxy_state.ftp and fixed_fState or 0
+    disable_socks_state = disable_state or proxy_state.type & 2 and not proxy_state.socks and fixed_fState or 0
+    auto_state = proxy_state.type == 2 and LISTEN_AUTO in proxy_state and fixed_fState or 0
+    gae_state = proxy_state.type == 2 and LISTEN_GAE in proxy_state  and fixed_fState or 0
+    sub_menu3 = (
+                 ('使用自动代理', on_enable_auto_proxy, auto_state, MFT_RADIOCHECK),
+                 ('使用 GAE 代理', on_enable_gae_proxy, gae_state, MFT_RADIOCHECK),
+                 ('完全禁用代理', on_disable_proxy, disable_state, MFT_RADIOCHECK),
                  (None, '-'),
-                 ('当前代理：', 'pass', MFS_DISABLED),
-                 (proxy_state, 'pass', MFS_DISABLED))
+                 ('禁用 HTTP 代理', on_disable_http_proxy, disable_http_state, MFT_RADIOCHECK),
+                 ('禁用 HTTPS 代理', on_disable_https_proxy, disable_https_state, MFT_RADIOCHECK),
+                 ('禁用 FTP 代理', on_disable_ftp_proxy, disable_ftp_state, MFT_RADIOCHECK),
+                 ('禁用 SOCKS 代理', on_disable_socks_proxy, disable_socks_state, MFT_RADIOCHECK))
     visible = ctypes.windll.user32.IsWindowVisible(hwnd)
-    show_item_state = visible and fixed_fState or 0
-    hide_item_state = not visible and fixed_fState or 0
+    show_state = visible and fixed_fState or 0
+    hide_state = not visible and fixed_fState or 0
     main_menu = (('GotoX 设置', sub_menu1, icon_gotox, MFS_DEFAULT),
                  ('更新直连 IP 库', sub_menu2),
                  (None, '-'),
-                 ('显示窗口', on_show, show_item_state, MFT_RADIOCHECK),
-                 ('隐藏窗口', on_hide, hide_item_state, MFT_RADIOCHECK),
+                 ('显示窗口', on_show, show_state, MFT_RADIOCHECK),
+                 ('隐藏窗口', on_hide, hide_state, MFT_RADIOCHECK),
                  ('设置系统（IE）代理', sub_menu3),
                  ('重启 GotoX', on_refresh),
                  (None, '-'),
                  ('关于', on_about))
-    global last_main_menu
     if main_menu != last_main_menu:
         systray.update(menu=main_menu)
         last_main_menu = main_menu
@@ -178,25 +317,29 @@ systray_GotoX = SysTrayIcon(icon_gotox, 'GotoX', None, quit_item,
 systray_GotoX.start()
 start_GotoX()
 #LISTEN_GAE, LISTEN_AUTO = get_listen_addr()
+
+from time import sleep
+
+proxy_state = get_proxy_state()
 sleep(1)
 systray_GotoX.update(
-    hover_text='GotoX\n当前系统（IE）代理：\n' + proxy_state,
+    hover_text='GotoX\n当前系统（IE）代理：\n%s' % proxy_state,
     balloons=('\nGotoX 已经启动。        \n\n'
               '左键单击：打开菜单\n\n'
               '左键双击：打开配置\n\n'
               '右键单击：隐藏窗口\n\n'
               '当前系统代理设置为：\n'
-              '    %s' % proxy_state,
+              '%s' % proxy_state,
               'GotoX 通知', 4 | 32, 15)
     )
 
 while True:
     now_proxy_state = get_proxy_state()
-    if proxy_state != now_proxy_state:
-        text = '设置由：\n      %s\n变更为：\n      %s' % (proxy_state, now_proxy_state)
+    if proxy_state.str != now_proxy_state.str:
+        text = '设置由：\n%s\n变更为：\n%s' % (proxy_state, now_proxy_state)
         proxy_state = now_proxy_state
         systray_GotoX.update(
-            hover_text='GotoX\n当前系统（IE）代理：\n' + proxy_state,
+            hover_text='GotoX\n当前系统（IE）代理：\n%s' % proxy_state,
             balloons=(text, '系统代理改变', 2 | 32, 15)
             )
     sleep(5)
